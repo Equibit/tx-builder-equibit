9253ba94fd1587a121b53e3185cc08cf6d0a22c3
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-08-01 20:38:18 +0300

    finished example of manual decoding

--- test/manual-decode.js
+++ test/manual-decode.js
@@ -15,75 +15,88 @@ const vinLen = varuint.decode(buffer, offset)
 console.log(`* vinLen = ${vinLen}, offset=${offset}, length=${varuint.decode.bytes}`)
 offset += varuint.decode.bytes
 
-const hashReversed = buffer.slice(offset, offset + 32)
-const hash = Buffer.from(hashReversed, 'hex').reverse().toString('hex')
-console.log(`* hash = ${hash.toString('hex')}, offset=${offset}, length=32`)
-offset += 32
+// VIN item:
+const readVin = (buffer, offset) => {
+  const hashReversed = buffer.slice(offset, offset + 32)
+  const hash = Buffer.from(hashReversed, 'hex').reverse().toString('hex')
+  console.log(`* hash = ${hash.toString('hex')}, offset=${offset}, length=32`)
+  offset += 32
 
-const index = buffer.readUInt32LE(offset)
-console.log(`* index = ${index}, offset=${offset}, length=4`)
-offset += 4
+  const index = buffer.readUInt32LE(offset)
+  console.log(`* index = ${index}, offset=${offset}, length=4`)
+  offset += 4
 
-const scriptLen = varuint.decode(buffer, offset)
-console.log(`* scriptLen = ${scriptLen}, offset=${offset}, length=${varuint.decode.bytes}`)
+  const scriptLen = varuint.decode(buffer, offset)
+  console.log(`* scriptLen = ${scriptLen}, offset=${offset}, length=${varuint.decode.bytes}`)
+  offset += varuint.decode.bytes
+
+  const script = buffer.slice(offset, offset + scriptLen)
+  console.log(`* script = ${script.toString('hex')}, offset=${offset}, length=${scriptLen}`)
+  offset += scriptLen
+
+  const sequence = buffer.readUInt32LE(offset)
+  console.log(`* sequence = ${sequence}, offset=${offset}, length=4`)
+  offset += 4
+
+  return offset
+}
+
+console.log('--- VIN-1 ---')
+offset = readVin(buffer, offset)
+console.log('--- VIN-2 ---')
+offset = readVin(buffer, offset)
+
+// VOUT
+const voutLen = varuint.decode(buffer, offset)
+console.log(`* voutLen = ${vinLen}, offset=${offset}, length=${varuint.decode.bytes}`)
 offset += varuint.decode.bytes
 
-const script = buffer.slice(offset, offset + scriptLen)
-console.log(`* script = ${script.toString('hex')}, offset=${offset}, length=${scriptLen}`)
-offset += scriptLen
+// VOUT item:
+const readVout = (buffer, offset) => {
+  const valueA = buffer.readUInt32LE(offset)
+  const valueB = buffer.readUInt32LE(offset + 4)
+  const valueBB = valueB * 0x100000000
+  const value = valueA + valueBB
+  console.log(`* value = ${value}, offset=${offset}, length=8`)
+  offset += 8
 
-const sequence = buffer.readUInt32LE(offset)
-console.log(`* sequence = ${sequence}, offset=${offset}, length=4`)
+  const lockingScriptLen = varuint.decode(buffer, offset)
+  console.log(`* lockingScriptLen = ${lockingScriptLen}, offset=${offset}, length=${varuint.decode.bytes}`)
+  offset += varuint.decode.bytes
+
+  const lockingScript = buffer.slice(offset, offset + lockingScriptLen)
+  console.log(`* lockingScript = ${lockingScript.toString('hex')}, offset=${offset}, length=${lockingScriptLen}`)
+  offset += lockingScriptLen
+
+  // equibit data:
+  const payment_currency = buffer.readUInt32LE(offset)
+  console.log(`* equibit.payment_currency = ${payment_currency}, offset=${offset}, length=4`)
+  offset += 4
+
+  const hashReversed = buffer.slice(offset, offset + 32)
+  const payment_tx_id = Buffer.from(hashReversed, 'hex').reverse().toString('hex')
+  console.log(`* equibit.payment_tx_id = ${payment_tx_id.toString('hex')}, offset=${offset}, length=32`)
+  offset += 32
+
+  const payloadLength = varuint.decode(buffer, offset)
+  console.log(`* payloadLength = ${payloadLength}, offset=${offset}, length=${varuint.decode.bytes}`)
+  offset += varuint.decode.bytes
+
+  const payload = buffer.slice(offset, offset + payloadLength)
+  console.log(`* payload = ${payload.toString()}, offset=${offset}, length=${varuint.decode.bytes}`)
+  offset += payloadLength
+
+  return offset
+}
+
+console.log('--- VOUT-1 ---')
+offset = readVout(buffer, offset)
+console.log('--- VOUT-2 ---')
+offset = readVout(buffer, offset)
+
+// Locktime
+const locktime = buffer.readUInt32LE(offset)
+console.log(`* locktime = ${locktime}, offset=${offset}, length=4`)
 offset += 4
-
-
-// // VOUT
-// const voutLen = varuint.decode(buffer, offset)
-// offset += varuint.decode.bytes
-//
-// // VOUT-1
-// const valueA = buffer.readUInt32LE(offset)
-// const valueB = buffer.readUInt32LE(offset + 4)
-// const valueBB = valueB * 0x100000000
-// const value = valueA + valueBB
-// offset += 8
-//
-// const lockingScriptLen = varuint.decode(buffer, offset)
-// offset += varuint.decode.bytes
-//
-// const lockingScript = buffer.slice(offset, offset + lockingScriptLen)
-// offset += lockingScriptLen
-//
-// // VOUT-2
-// const valueA2 = buffer.readUInt32LE(offset)
-// const valueB2 = buffer.readUInt32LE(offset + 4)
-// const valueBB2 = valueB2 * 0x100000000
-// const value2 = valueA2 + valueBB2
-// offset += 8
-//
-// const lockingScriptLen2 = varuint.decode(buffer, offset)
-// offset += varuint.decode.bytes
-//
-// const lockingScript2 = buffer.slice(offset, offset + lockingScriptLen2)
-// offset += lockingScriptLen2
-//
-// // Locktime
-// const locktime = buffer.readUInt32LE(offset)
-// offset += 4
-//
-//
-// console.log(`voutLen = ${voutLen}`)
-// console.log(`valueA = ${valueA}`)
-// console.log(`valueB = ${valueB}`)
-// console.log(`valueB2 = ${valueBB}`)
-// console.log(`value = ${value}`)
-// console.log(`lockingScriptLen = ${lockingScriptLen}`)
-// console.log(`lockingScript = ${lockingScript.toString('hex')}`)
-// console.log(`value2 = ${value2}`)
-// console.log(`lockingScriptLen2 = ${lockingScriptLen2}`)
-// console.log(`lockingScriptLen2 = ${lockingScriptLen2}`)
-// console.log(`lockingScript2 = ${lockingScript2.toString('hex')}`)
-// console.log(`locktime = ${locktime}`)
-//
 
 console.log(`BUFFER LEFT = ${buffer.slice(offset).toString('hex')}`)
