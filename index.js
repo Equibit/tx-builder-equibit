c70f00b5f2d0dbfdaf6e64ddb3964c631e9214ec
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-08-31 17:57:27 -0400

    0.3.0 Upgraded tx-builder; restructured export

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const { decodeTx } = require('./src/tx-decoder-equibit')
+const decoder = require('./src/tx-decoder-equibit')
+const builder = require('./src/tx-builder-equibit')
+const { getTxId } = require('tx-builder/src/tx-decoder')
 
-module.exports = decodeTx
+module.exports = {
+  decoder,
+  builder,
+  getTxId
+}
--- tx-decoder.js
+++ tx-decoder.js
@@ -1,1 +1,1 @@
-module.exports = require('./src/tx-decoder-equibit').decodeTx
+module.exports = require('./src/tx-decoder-equibit')
