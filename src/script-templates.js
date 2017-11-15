b71ec150ada217afb21a037a398c80e026c1d91e
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-11-14 23:36:46 -0500

    explaining transactions of atomic trade

--- src/script-templates.js
+++ src/script-templates.js
@@ -12,3 +12,28 @@ module.exports = {
     `
   }
 }
+
+// scriptPubKey:
+// Buyer: `signature publicKey secretX`
+// Seller `signature publicKey`
+
+
+// Ilya Buyer:
+// - generate SecretX, + hashSecret
+// TX1 Bitcoin: if (hashSecret && Sergey) else (timelock 72 && Ilya)
+
+// Sergey Seller:
+// Tx2 Equibit: if (hashSecret && IlyaEqb1) else (timelock 24 && SergeyEqb1)
+
+// Ilya: claim equibit revealing secret:
+// Tx3 Equibit: TX2 -> (signature Ilya pubKey SecretX) to IlyaEqb2 address
+
+// Sergey: claim bitcoin using secret:
+// TX4 Bitcoin: TX1 -> (Sergey signature pubKey SecretX) -> to Sergey address
+
+
+// Passport:
+// "IlyaEqb1 SignedBy PassportAuthority" === "<equibit Ilya address> <pubKey Authority> <signature Authority>"
+
+// IBM Issuer:
+// "PassportAuthorityPubKey SignedBy IBM" === "<equibit Authority address> <pubKey IBM> <signature IBM>"
