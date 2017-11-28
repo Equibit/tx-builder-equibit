8c4a8457205d17cee1227addf2b976b52f7ceb28
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-11-27 23:30:11 -0500

    added hashTimelockContract to main export

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const decoder = require('./src/tx-decoder-equibit')
 const builder = require('./src/tx-builder-equibit')
 const { getTxId } = require('tx-builder/src/tx-decoder')
+const { hashTimelockContract, generateSecret } = require('./src/script-builder')
 
 module.exports = {
   decoder,
   builder,
-  getTxId
+  getTxId,
+  hashTimelockContract,
+  generateSecret
 }
